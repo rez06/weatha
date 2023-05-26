a4da0dbc2e55c8f47b406703ba6123840f3575dc
rez06 <pelobello.rezyl@gmail.com>
2023-05-27 00:08:45 +0800

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weatha app
+      <h1>Weatha App</h1>
+      <button className='btn btn-primary'>hello</button>
+
+      <p>Coded by <a href="https://github.com/rez06/weatha">Rezyl Pelobello</a></p>
     </div>
   );
 }
